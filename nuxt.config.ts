b55b771acca7b3b35c0b6efda290c415e4c5f0c4
ddahan <david.dahan3@gmail.com>
2023-07-29 08:26:34 +0000

    Using nuxtlabs UI edge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxthq/ui"],
+  modules: ["@nuxthq/ui-edge"],
   ui: {
     global: true,
     icons: "all",
