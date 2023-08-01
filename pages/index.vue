803b6c5e8339a5c91d113dcbbb9a8c6b6eb50626
ddahan <david.dahan3@gmail.com>
2023-08-01 09:35:11 +0000

    meta tags seems to have no effect

--- pages/index.vue
+++ pages/index.vue
@@ -41,13 +41,6 @@ const title = ref("UI Lib Picker");
 
 useHead({
   title,
-  meta: [
-    {
-      name: "viewport",
-      content:
-        "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
-    },
-  ],
   link: [
     {
       rel: "icon",
