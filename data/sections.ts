a694a974397f575b73fd541ae686da3eccfbf2c7
ddahan <david.dahan3@gmail.com>
2023-08-18 08:59:18 +0000

    Add new components to data

--- data/sections.ts
+++ data/sections.ts
@@ -2,18 +2,110 @@ export const sections: Array<Section> = [
   {
     name: "Elements",
     components: [
+      { name: "Accordion", description: "Vertically collapsing accordion panels" },
       { name: "Avatar", description: "Rounded image of a resource" },
       {
         name: "Badge",
         aliases: ["Chip"],
-        description: "Lorem ipsum blabla",
+        description: "Short text to represent a status or a category",
       },
+      { name: "Button", description: "" },
+      { name: "Button Group", description: "Group a series of buttons together" },
+      {
+        name: "Dropdown",
+        description: "Toggle contextual overlays for displaying lists of actions",
+      },
+      { name: "Keyboard Key", description: "A keyboard key in a text block" },
     ],
   },
   {
     name: "Forms",
-    components: [{ name: "Input", description: "todo" }],
+    components: [
+      { name: "Input", description: "An input field" },
+      { name: "Textarea", description: "A textarea field" },
+      { name: "Select", description: "A select field" },
+      {
+        name: "Select Menu",
+        description:
+          "A select menu with advanced features (search, multiple select, etc.)",
+      },
+      { name: "Checkbox", description: "A checkbox field" },
+      { name: "Radio", description: "A radio field" },
+      { name: "Toggle", description: "A toggle field" },
+      { name: "Range", description: "A range field" },
+      {
+        name: "Form Group",
+        description: "A label and additional informations around a form element",
+      },
+      { name: "Form", description: "Form data collection and validation" },
+    ],
+  },
+  {
+    name: "Data",
+    components: [
+      {
+        name: "Table",
+        description: "Data displayed in a table",
+      },
+    ],
+  },
+  {
+    name: "Navigation",
+    components: [
+      {
+        name: "Vertical Navigation",
+        description: "",
+      },
+      {
+        name: "Command Palette",
+        description: "",
+      },
+      {
+        name: "Pagination",
+        description: "",
+      },
+      {
+        name: "Tabs",
+        description: "A set of tab panels that are displayed one at a time",
+      },
+    ],
+  },
+  {
+    name: "Overlays",
+    components: [
+      {
+        name: "Modal",
+        description: "",
+      },
+      {
+        name: "Slideover",
+        description: "",
+      },
+      {
+        name: "Popover",
+        description: "",
+      },
+      {
+        name: "Tooltip",
+        description: "",
+      },
+      {
+        name: "Context Menu",
+        description: "A dedicated menu that appears on right click",
+      },
+      {
+        name: "Toast",
+        aliases: ["Notification", "Alert"],
+        description: "",
+      },
+    ],
+  },
+  {
+    name: "Layout",
+    components: [
+      { name: "Card", description: "" },
+      { name: "Container", description: "" },
+      { name: "Skeleton", description: "" },
+    ],
   },
 ];
-
-// TODO: need a util method to flaten the components, without the sections.
--- types/components.types.ts
+++ types/components.types.ts
@@ -1,8 +1,52 @@
 // component sections
-type SectionName = "Elements" | "Forms";
+// TODO: replace by key of?
+type SectionName = "Elements" | "Forms" | "Data" | "Navigation" | "Overlays" | "Layout";
 
 // components
-type ComponentName = "Avatar" | "Badge" | "Input";
+// TODO: replace by key of?
+type ComponentName =
+  // Elements
+  | "Accordion"
+  | "Avatar"
+  | "Badge"
+  | "Button"
+  | "Button Group"
+  | "Dropdown"
+  | "Keyboard Key"
+
+  // Forms
+  | "Input"
+  | "Textarea"
+  | "Select"
+  | "Select Menu"
+  | "Checkbox"
+  | "Radio"
+  | "Toggle"
+  | "Range"
+  | "Form Group"
+  | "Form"
+
+  // Data
+  | "Table"
+
+  // Navigation
+  | "Vertical Navigation"
+  | "Command Palette"
+  | "Pagination"
+  | "Tabs"
+
+  // Overlays
+  | "Modal"
+  | "Slideover"
+  | "Popover"
+  | "Tooltip"
+  | "Context Menu"
+  | "Toast"
+
+  // Layout
+  | "Card"
+  | "Container"
+  | "Skeleton";
 
 type Section = { name: SectionName; components: Component[] };
 
