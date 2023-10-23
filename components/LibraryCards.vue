52ad85dd3f1f20a6d1f7c10f854a61b93d6830ce
LouisMazel <me@loicmazuel.com>
2023-10-23 12:01:50 -0400

    feat: add maz-ui library

--- components/LibraryCards.vue
+++ components/LibraryCards.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="grid grid-cols-1 lg:grid-cols-2 gap-6 flex-grow content-start">
-    <LibraryCard
-      v-for="library in libraries"
-      :initialLibrary="library"
-    />
+    <LibraryCard v-for="library in sortedLibraries" :initialLibrary="library" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { libraries } from "@/data/libraries";
+import { sortedLibraries } from "@/data/libraries";
 </script>
